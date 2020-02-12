691556190e0af43d0d98a38ae0c4d16410789c26
sridhar-alt <sridhar-alt@users.noreply.github.com>
2020-02-12 18:15:25 +0530

    Add project files.

---
+++ MvcSample/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSample.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public string Index()
+        {
+            return "login";
+        }
+    }
+}
